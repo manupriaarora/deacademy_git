0005ae4ec47cd7b1758afae0b159ce9ac44954b0
Manupriya Arora <manupriaarora@gmail.com>
2025-08-03 01:51:01 +0000

    Short yellow project - SCD 2 changes using DBT

--- macros/copy_into_snowflake.sql
+++ macros/copy_into_snowflake.sql
@@ -1,18 +1,45 @@
-{% macro copy_json(table_nm) %}
+{% macro macros_copy_csv(table_nm) %} 
 
---Delete the data from the copy table before running the copy command
-delete from {{var ('target_db') }}.{{var ('target_schema')}}.{{ table_nm }};
+delete from {{var ('rawhist_db') }}.{{var ('wrk_schema')}}.{{ table_nm }};
 
---Copy the data from the snowflake external stage to snowflake table
-COPY INTO {{var ('target_db') }}.{{var ('target_schema')}}.{{ table_nm }}
-FROM
+COPY INTO {{var ('rawhist_db') }}.{{var ('wrk_schema')}}.{{ table_nm }} 
+
+FROM 
 (
 SELECT
-$1 AS DATA
+
+    $1 AS ProductId,
+
+    $2 AS ProductName,
+
+    $3 AS Category,
+
+    $4 AS SellingPrice,
+
+    $5 AS ModelNumber,
+
+    $6 AS AboutProduct,
+
+    $7 AS ProductSpecification,
+
+    $8 AS TechnicalDetails,
+
+    $9 AS ShippingWeight,
+
+    $10 AS ProductDimensions,
+
+    CURRENT_TIMESTAMP() AS INSERT_DTS,
+
+    CURRENT_TIMESTAMP() AS UPDATE_DTS,
+
+    metadata$filename AS SOURCE_FILE_NAME,
+
+    metadata$file_row_number AS SOURCE_FILE_ROW_NUMBER
+
 FROM @{{ var('stage_name') }}
 )
-FILE_FORMAT = (TYPE = JSON)
+FILE_FORMAT = {{ var ('file_format_json') }}
+PURGE={{ var('purge_status') }}
 FORCE = TRUE;
 
 {% endmacro %}
-
